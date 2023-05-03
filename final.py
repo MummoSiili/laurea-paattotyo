c7ec15535965ceab649bb33a8c9e1ea999416d61
MummoSiili <tanner.ari@outlook.com>
2023-05-03 11:44:48 +0300

    Päättötyö valmis

--- final.py
+++ final.py
@@ -1,17 +1,38 @@
 import sys, pickle, time
 
-lista = []
-
 # Kirjoita teksti ja palauta se aikaleimalla
 def add_value():
     teksti = input('Kirjoita uusi merkintä: ')
-    teksti = teksti + ':::' + time.strftime('%X %x') +'\n'
+    teksti = teksti + ':::' + time.strftime('%X %x')
     return teksti
+
+def change_marking(lista, lista_index):
+    print(lista[lista_index])
+    print('Anna uusi teksti:', end='')
+    teksti = input()
+    teksti = teksti + ':::' + time.strftime('%X %x')
+    lista[lista_index] = teksti
+    return lista
+
+def delete_marking(lista, lista_index):
+    print(f'Poistettiin merkintä {lista[lista_index]}')
+    lista.pop(lista_index)
+    return lista
 
 def main():
     # Testataan onko muistio.dat olemassa
-    
+    try:
+        file = open('muistio.dat', 'rb')
+        lista = pickle.load(file)
+        file.close()
+    except:
+        print('Virhe tiedostossa, luodaan uusi muistio.dat.')
 
+        with open('muistio.dat', 'wb') as file:
+            lista = []
+            pickle.dump(lista, file)
+
+    # Suoritetaan ohjelmaa niin kauan kunnes valitaan lopeta.
     while True:
 
         # Tulostaa valikon
@@ -28,18 +49,25 @@ Mitä haluat tehdä?: ''', end='')
 
         # Valikon vaihtoehdot käsitellään str. Ei käännöstä int-muotoon
         if valinta == '1':
-            with open('muistio.dat', 'rb') as file:
-                sisalto = pickle.load(file)
-            print(sisalto)
+            for value in lista:
+                print(value, end='')
         elif valinta == '2':
             lista.append(add_value())
         elif valinta == '3':
-            pass
+            print(f'Listalla on {len(lista)} merkintää.')
+            print('Mitä niistä muutetaan?: ', end='')
+            lista_index = int(input())
+            lista_index = lista_index - 1
+            lista = change_marking(lista, lista_index)
         elif valinta == '4':
-            pass
+            print(f'Listalla on {len(lista)} merkintää.')
+            print('Mitä niistä poistetaan?: ', end='')
+            lista_index = int(input())
+            lista_index = lista_index - 1
+            lista = delete_marking(lista, lista_index)
         elif valinta == '5':
             with open('muistio.dat', 'wb') as file:
-                file.write(pickle.dump(lista))
+                pickle.dump(lista, file)
             print('Lopetetaan.')
             sys.exit(0)
 
